c8718b17110ddee60585cd500255cebd50cc5d52
BLADERUNNER\baker <jay@palepachyderm.com>
2025-05-26 15:24:40 +1200

    Fixed the Day view

--- ignition/named-query/Weight_Q/Weight_Day/query.sql
+++ ignition/named-query/Weight_Q/Weight_Day/query.sql
@@ -4,7 +4,7 @@ SELECT
     weight_sum, 
     weight_max,
     weight_min, 
-    count, 
+    [count], 
     pct_out_of_range, 
     standard_deviation, 
     weight_range, 
@@ -18,12 +18,22 @@ SELECT
     sp_plc,
     sp_low_plc,
     sp_high_plc,
-    (select STRING_AGG(material, ', ') from weight.dbo.aggregated a2 where a2.scale_id = a.scale_id and a2.time_start = a.time_start and a2.material != 'All') as material
+    'material' = 
+    CASE :material
+    WHEN 'All' THEN
+        (	select STRING_AGG(material, ', ') 
+    	from weight.dbo.aggregated a2 
+    	where a2.scale_id = a.scale_id 
+    	and a2.time_start = a.time_start 
+    	and a2.material != 'All')
+    ELSE material
+    END
 FROM 
     weight.dbo.aggregated a
 WHERE 
-    scale_id = :scale_id AND 
     time_start >= :start AND 
-    time_start < DATEADD(DAY, 1, :start)
+    time_start < DATEADD(DAY, 1, :start) AND
+    scale_id = :scale_id AND 
+	material = :material
 ORDER BY 
     time_start;
