46269e311cc75e16ea6bdce6974ef7a155ffe128
BLADERUNNER\baker <jay@palepachyderm.com>
2025-05-30 15:15:29 +1200

    Now calculates rejects by material as well as a general version.

--- ignition/named-query/Weight_Q/Rejects_Q/UpsertReject/query.sql
+++ ignition/named-query/Weight_Q/Rejects_Q/UpsertReject/query.sql
@@ -1,8 +1,7 @@
 update  weight.dbo.rejects 
 set metal_count = :metal_count, 
-	weight_count = :weight_count, 
-	material = :material
-where line_id = :line_id and time_start = :time_start
+	weight_count = :weight_count
+where line_id = :line_id and time_start = :time_start and material = :material
 
 if @@ROWCOUNT = 0
 	insert into weight.dbo.rejects (line_id, time_start, metal_count, weight_count, material)
