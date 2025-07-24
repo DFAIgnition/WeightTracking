cc86129ba4bc6d37d0dce131eeb220472a4f5a2b
BLADERUNNER\baker <jay@palepachyderm.com>
2025-07-24 15:37:23 +1200

    Added changes to the Overview screen. Might need a bit more testing? Seems pretty close, though

--- ignition/named-query/Weight_Q/DB_Query/Get_Scale_Info/query.sql
+++ ignition/named-query/Weight_Q/DB_Query/Get_Scale_Info/query.sql
@@ -1,8 +1,7 @@
 SELECT 			f.filler_design, f.filler_sp, u.unit_name, u.unit_conversion,s.scale_name
-				
 FROM 			weight.dbo.filler as f
-
 INNER JOIN 		weight.dbo.scale AS s ON f.filler_id = s.filler_id
 INNER JOIN 		weight.dbo.unit AS u ON f.unit_id = u.unit_id
-
 WHERE			scale_id =  :scale_id 
+or (:scale_id  = 0 and f.line_id = :line_id)
+order by s.scale_name
