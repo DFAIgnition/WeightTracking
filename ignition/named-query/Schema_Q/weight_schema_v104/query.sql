74692273e41399513a985bc85cb61547c9a67bd5
BLADERUNNER\baker <jay@palepachyderm.com>
2025-07-24 16:56:57 +1200

    Version for release

---
+++ ignition/named-query/Schema_Q/weight_schema_v104/query.sql
@@ -0,0 +1,20 @@
+--------------------------------------------------------------------------------
+-- Use weight DB
+--------------------------------------------------------------------------------
+
+USE [weight];
+GO
+
+alter table unit
+add unit_description nvarchar(32);
+
+update unit set unit_description = 'Grams' where unit_name = 'g';
+update unit set unit_description = 'Pounds' where unit_name = 'lbs';
+update unit set unit_description = 'Kilograms' where unit_name = 'kg';
+update unit set unit_description = 'Ounces' where unit_name = 'oz';
+
+--------------------------------------------------------------------------------
+-- Versions table update
+--------------------------------------------------------------------------------
+INSERT INTO weight.dbo.versions (version_number) VALUES (1.04);
+GO
--- ignition/script-python/Constants_P/code.py
+++ ignition/script-python/Constants_P/code.py
@@ -1,14 +1,15 @@
 PROJECT={
     'project_name':system.project.getProjectName(),
-    'db_name':'Weight',
-    'version_number':1.10,
+    'db_name':'weight',
+    'version_number':1.11,
     'project_dependencies':{
         'Core':1.54,
         'STARR':1.62,
         'Admin':1.26
     },
     'db_dependencies':{
-        'system':1.26        
+        'system':1.26,
+        'weight':1.04
     }
 }    
  
